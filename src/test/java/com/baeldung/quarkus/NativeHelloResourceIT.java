8ce41f3f6f8c146605ecda70eb8ea0be1bc2b075
didier <durand.didier@gmail.com>
2020-08-18 08:07:18 +0200

    upgrading annotation based on https://github.com/quarkusio/quarkus/issues/4989

--- src/test/java/com/baeldung/quarkus/NativeHelloResourceIT.java
+++ src/test/java/com/baeldung/quarkus/NativeHelloResourceIT.java
@@ -1,8 +1,8 @@
 package com.baeldung.quarkus;
 
-import io.quarkus.test.junit.SubstrateTest;
+import io.quarkus.test.junit.NativeImageTest;
 
-@SubstrateTest
+@NativeImageTest
 public class NativeHelloResourceIT extends HelloResourceTest {
 
     // Execute the same tests but in native mode.
